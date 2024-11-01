b488671e84857ce408d3910783f23a5ad4d1fdcf
joel <joel@burgerjoe.dev>
2024-11-01 19:21:35 +1300

    codecrafters submit [skip ci]

--- app/main.js
+++ app/main.js
@@ -1,5 +1,6 @@
 const process = require('process');
 const util = require('util');
+const fs = require('fs');
 const { decodeBencode } = require('./decoder');
 
 function main() {
@@ -7,6 +8,12 @@ function main() {
   if (command === 'decode') {
     const bencodedValue = process.argv[3];
     console.log(JSON.stringify(decodeBencode(bencodedValue)));
+  } else if (command === 'info') {
+    const inputFile = process.argv[3];
+    const bencodedValue = fs.readFileSync(inputFile, 'utf8');
+    const decoded = decodeBencode(bencodedValue);
+    console.log(`Tracker URL: ${decoded.announce}`);
+    console.log(`Length: ${decoded.info.length}`);
   } else {
     throw new Error(`Unknown command ${command}`);
   }
